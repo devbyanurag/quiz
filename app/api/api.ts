59d38129c48283b31c36a874d47f44dbba0c40b5
anuragchhetri <karanchheti@gmail.com>
2024-02-05 19:30:54 +0530

    checked production url

--- app/api/api.ts
+++ app/api/api.ts
@@ -2,8 +2,8 @@ import axios, { AxiosInstance } from 'axios';
 import { BASE_URL_BACKEND_LOCAL, BASE_URL_BACKEND_SERVER } from '../utils/constants';
 
 const isDev = process.env.NODE_ENV
-const baseURL = isDev ? BASE_URL_BACKEND_LOCAL : BASE_URL_BACKEND_SERVER;
-alert(process.env.NODE_ENV)
+const baseURL = isDev==='development' ? BASE_URL_BACKEND_LOCAL : BASE_URL_BACKEND_SERVER;
+
 export const api_backend: AxiosInstance = axios.create({
     baseURL: baseURL,
 });
